e89564b4b3f1bf8fdf06ea6f043efb27e100e6d5
tri-dip <kalitaminu464@gmail.com>
2025-07-13 18:50:50 +0530

    fixes in get

--- backend/index.js
+++ backend/index.js
@@ -168,13 +168,6 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
